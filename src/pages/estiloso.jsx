1243c1e19a91c0859a8d455f9ed59c422edadb6a
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-15 09:35:59 -0300

    created a module css example.

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,10 @@
+import styles from '../styles/Estiloso.module.css';
+
+export default function Estiloso() {
+    return (
+      <div className={styles.roxo}>
+        <h1>JSX é um conceito Central</h1>
+      </div>
+    );
+  }
+  
