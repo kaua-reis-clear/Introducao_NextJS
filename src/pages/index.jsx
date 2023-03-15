319970853f083b1eace5c39715e5809b120fe777
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-15 10:59:26 -0300

    added component with use state, and addeds examples of apis using query parameters.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -14,6 +14,7 @@ export default function Inicio() {
         <Navegador texto="JSX" destino="/jsx" cor="crimson"/>
         <Navegador texto="Navegacao #01" destino="/navegacao" cor="green"/>
         <Navegador texto="Navegacao #02" destino="/cliente/sp-2/123" cor="blue"/>
+        <Navegador texto="Componente com Estado" destino="/estado" cor="pink"/>
     </div>
   );
 }
