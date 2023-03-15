dba3992230bf2571a3d80afa1aab6284c09b3bdf
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-15 10:01:46 -0300

    created more examples of module css.

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,10 +1,14 @@
 import styles from '../styles/Estiloso.module.css';
+import Link from 'next/link';
+import Layout from '../components/Layout';
 
 export default function Estiloso() {
     return (
-      <div className={styles.roxo}>
-        <h1>JSX é um conceito Central</h1>
-      </div>
+      <Layout titulo="Exemplo de CSS Modularizado">
+        <div className={styles.roxo}>
+          <h1>JSX é um conceito Central</h1>
+        </div>
+      </Layout>
     );
   }
   
